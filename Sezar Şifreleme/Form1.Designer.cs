825a2e695e0a297159d7d9cd56eba8d1f4f4bbe3
Mahmut Baysağ <mbaysag1080@gmail.com>
2021-12-04 19:21:51 +0300

    update

---
+++ Sezar Şifreleme/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace Sezar_Şifreleme
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button1.Font = new System.Drawing.Font("Trebuchet MS", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(22, 97);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 43);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Şifrele";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.CharacterCasing = System.Windows.Forms.CharacterCasing.Lower;
+            this.textBox1.Font = new System.Drawing.Font("Malgun Gothic Semilight", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(145, 22);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(407, 43);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button2.Font = new System.Drawing.Font("Trebuchet MS", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(22, 170);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(530, 62);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Kaydet";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button3.Font = new System.Drawing.Font("Trebuchet MS", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.Location = new System.Drawing.Point(22, 238);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(530, 62);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Getir";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox2.CharacterCasing = System.Windows.Forms.CharacterCasing.Lower;
+            this.textBox2.Font = new System.Drawing.Font("Malgun Gothic Semilight", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox2.Location = new System.Drawing.Point(140, 98);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(407, 43);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Harrington", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(16, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 32);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Veri Gir";
+            // 
+            // textBox3
+            // 
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox3.CharacterCasing = System.Windows.Forms.CharacterCasing.Lower;
+            this.textBox3.Font = new System.Drawing.Font("Malgun Gothic Semilight", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox3.Location = new System.Drawing.Point(145, 343);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(407, 43);
+            this.textBox3.TabIndex = 6;
+            // 
+            // button4
+            // 
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button4.Font = new System.Drawing.Font("Trebuchet MS", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button4.Location = new System.Drawing.Point(22, 345);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(112, 43);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Çöz";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(581, 441);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Sezar Şifreleme";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Sezar Şifreleme/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using Microsoft.VisualBasic;
+
+namespace Sezar_Şifreleme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string bilgi = textBox1.Text;
+            bilgi.ToLower();
+
+            if (textBox1.Text != "" || textBox1.Text != null)
+            {
+                int i = 0;
+                string yedek = "";
+                char karakter;
+                while (i < bilgi.Length)
+                {
+                    karakter = bilgi[i];                  
+                    yedek += sifrele(bilgi,karakter,i);
+                    i++;
+                }
+                textBox2.Text = yedek;
+            }
+            else
+                MessageBox.Show("Lütfen sifrelenecek veri bilgi giriniz");
+        }
+
+        char sifrele(string bilgi,char karakter,int i)
+        {
+            string alfabe = "abcçdefgğhıijklmnoöprsştuüvyz";
+            string rakamlar = "0123456789";
+            if (!char.IsNumber(karakter) && (!char.IsSymbol(karakter) && !char.IsSurrogate(karakter)))
+            {
+                if (karakter == 'v' || karakter == 'y' || karakter == 'z')
+                {
+                    return alfabe[alfabe.IndexOf(bilgi[i]) - 26];
+                }
+                else if (karakter == ' ')
+                {
+                    return ' ';
+                }
+                else
+                {
+                    return alfabe[alfabe.IndexOf(bilgi[i]) + 3];
+                }
+            }
+            else if(!char.IsSymbol(karakter) && !char.IsSurrogate(karakter))
+            {
+                if (karakter == '7' || karakter == '8' || karakter == '9')
+                {
+                    return rakamlar[rakamlar.IndexOf(bilgi[i]) - 7];
+                }
+                else if (karakter == ' ')
+                {
+                    return ' ';
+                }
+                else
+                {
+                    return rakamlar[rakamlar.IndexOf(bilgi[i]) + 3];
+                }
+            }
+            else
+            {
+                MessageBox.Show("Lütfen Harfler ve Rakamlardan oluşan bilgiler giriniz", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                //bu kısımda döngüden çıkmaya bak
+            }
+            return ' ';
+        
+        }
+
+        char sifreCoz(string bilgi, char karakter, int i)
+        {
+            string alfabe = "abcçdefgğhıijklmnoöprsştuüvyz";
+            string rakamlar = "0123456789";
+            if (!char.IsNumber(karakter))
+            {
+                if (karakter == 'a' || karakter == 'b' || karakter == 'c')
+                {
+                    return alfabe[alfabe.IndexOf(bilgi[i]) + 26];
+                }
+                else if (karakter == ' ')
+                {
+                    return ' ';
+                }
+                else
+                {
+                    return alfabe[alfabe.IndexOf(bilgi[i]) - 3];
+                }
+            }
+            else
+            {
+                if (karakter == '0' || karakter == '1' || karakter == '2')
+                {
+                    return rakamlar[rakamlar.IndexOf(bilgi[i]) + 7];
+                }
+                else if (karakter == ' ')
+                {
+                    return ' ';
+                }
+                else
+                {
+                    return rakamlar[rakamlar.IndexOf(bilgi[i]) - 3];
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text != "" || textBox2.Text != null)
+            {
+                Registry.CurrentUser.SetValue("sezarsifreleme", textBox2.Text);
+                MessageBox.Show("Şifreli bilgi başarıyla kaydedildi", "Başarılı");
+            }
+            else
+                MessageBox.Show("Lütfen şifrelenecke bilgi giriniz");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string bilgi = Registry.CurrentUser.GetValue("sezarsifreleme").ToString();
+            int i = 0;
+            string yedek = "";
+            char karakter;
+            while (i < bilgi.Length)
+            {
+                karakter = bilgi[i];
+                yedek += sifreCoz(bilgi,karakter,i);
+                i++;
+            }
+            MessageBox.Show(yedek,"Başarılı");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string bilgi = textBox3.Text;
+            int i = 0;
+            string yedek = "";
+            char karakter;
+            while (i < bilgi.Length)
+            {
+                karakter = bilgi[i];
+                yedek += sifreCoz(bilgi,karakter,i);
+                i++;
+            }
+            MessageBox.Show(yedek, "Başarılı");
+        }
+    }
+}
